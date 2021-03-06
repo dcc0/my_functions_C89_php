5cfd3042440cbba2c24ec4ebd0fb6d5249a120bc
dcc0 <ivangavr777@gmail.com>
2021-03-06 13:10:18 +0300

    Update finite_state_n.c

--- finite_state_n.c
+++ finite_state_n.c
@@ -6,32 +6,31 @@
 
 int test(char * ip) {
 
-        if(system(ip)==0) {	
-	return 1;			
+    if(system(ip)==0) {
+	return 1;
 }
 	else {
 	return 0;
    }
-		
-}	
+}
 
 void wait(int b, char * ip) {
 
-	while (1) {	
-	
+	while (1) {
+
 	sleep (5);
 	test(ip);
-		
+
 	if (b!=test(ip)) {
 	b=test(ip);
-		
+
 	if (test(ip) == 1) {
 	system("beep -f 2200 -l 3000");
-} 
+}
 	else {
 	system("beep -f 1200 -l 2000");
-		
-	    }	
+
+	    }
 	}
     }
 }
@@ -39,24 +38,24 @@ void wait(int b, char * ip) {
 
 
 void start (char * ip) {
-	
+
 	if (test(ip)==0)
 	system("beep -f 1200 -l 2000");
-	
+
 }
 
 
 int main (int argc, char *argv[]) {
-             
+
 	if (argc < 2) {
         printf("Введите адрес  вторым параметром ");
-        return 0;         
-} 
-		
+        return 0;
+}
+
 	char ip[100]="ping -c 1 ";
-	sprintf(ip, "%s%s", ip, argv[1]); 
-	    
+	sprintf(ip, "%s%s", ip, argv[1]);
+
 	int b = 0;
 	start (ip);
-	wait (b, ip);	
+	wait (b, ip);
 }
